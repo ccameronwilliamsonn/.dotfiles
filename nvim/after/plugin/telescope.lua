8c9238ee8c2451b9223fbdb086ecadd581314d2b
Cameron Williamson <cwilliamson@transformd.com>
2023-10-23 10:44:19 +1100

    add telescope mapping for buffers and sort by recent

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,6 +2,19 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<leader>pr', builtin.lsp_references, {})
+
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {} )
+
+require('telescope').setup({
+    pickers = {
+        buffers = {
+            show_all_buffers = true,
+            sort_mru = true,
+            ignore_current_buffer = true,
+        }
+    }
+})
+
